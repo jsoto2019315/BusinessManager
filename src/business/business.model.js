b939d427fcc0577e37c4aa6f4e0e781b0eef176a
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-03-02 14:16:48 -0600

    Fields for Business were created to be worked on in a future

---
+++ src/business/business.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const BusinessSchema = mongoose.Schema({
+    businessName:{
+        type: String,
+        require: [true, "Required field"]
+    },
+    impactLevel:{
+        type: String,
+        require: [true, "Required field"]
+    },
+    category:{
+        type: String,
+        require: [true, "Required field"]
+    },
+    years:{
+        type: Date,
+        require: [true, "Required field"]
+    },
+    status:{
+        type: Boolean,
+        default: true
+    }
+})
+
+BusinessSchema.methods.toJSON = function(){
+    const { __v, _id, status, ...business } = this.toObject();
+    business.uid = _id;
+    return business;
+}
+
+export default mongoose.model('Business', BusinessSchema);
